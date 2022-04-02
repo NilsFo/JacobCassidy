61fb14a0523e9997b3af39e949ff8ab5bebf17d4
PatrickQ <Patrick@Quiskamp.de>
2022-04-02 14:22:46 +0200

    Bullets

--- Assets/Scripts/Behaviours/BulletBehaviourScript.cs
+++ Assets/Scripts/Behaviours/BulletBehaviourScript.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class BulletBehaviourScript : MonoBehaviour
 {
+    public float speed = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
