457a1c26d2c9afe36709b8a4b0e24cc25404013a
PatrickQ <Patrick@Quiskamp.de>
2022-04-02 17:25:49 +0200

    UI Dev

---
+++ Assets/Scripts/UI/StaminaBehaviourScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class StaminaBehaviourScript : MonoBehaviour
+{
+    [SerializeField] private PlayerStateBehaviourScript _playerStateBehaviourScript;
+    
+    [SerializeField] private TextMeshProUGUI goText;
+    
+    private void Awake()
+    {
+        Debug.Assert(_playerStateBehaviourScript != null, gameObject);
+        Debug.Assert(goText != null, gameObject);
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerStateBehaviourScript.onCurrentStaminaChange.AddListener(UpdateStaminaText);
+    }
+
+    private void OnDisable()
+    {
+        _playerStateBehaviourScript.onCurrentStaminaChange.RemoveListener(UpdateStaminaText);
+    }
+
+    public void UpdateStaminaText()
+    {
+        goText.SetText(Mathf.Round(_playerStateBehaviourScript.CurrentStamina) + "/" + _playerStateBehaviourScript.MAXStamina);
+    }
+}
